c63521743b7a55e1f97eb2fc1c90362ba54b5da4
Frédéric Legrand <frederic@phospho.ai>
2025-04-12 12:25:25 +0200

    feat: add double so100 with 4 cams setup

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -504,6 +504,104 @@ class DoubleSo100WCDataConfig(BaseDataConfig):
 ###########################################################################################
 
 
+class DoubleSo100W2CDataConfig(BaseDataConfig):
+    """
+    phospho setup with 2 SO100 arm, 2 context cams and 2 wrist cams
+    """
+
+    video_keys = ["video.cam_context_1", "video.cam_context_2", "video.cam_wrist_left", "video.cam_wrist_right"]
+    state_keys = [
+        "state.left_arm",
+        "state.left_gripper",
+        "state.right_arm",
+        "state.right_gripper",
+    ]
+    action_keys = [
+        "action.left_arm",
+        "action.left_gripper",
+        "action.right_arm",
+        "action.right_gripper",
+    ]
+    language_keys = ["annotation.human.task_description"]
+    observation_indices = [0]
+    action_indices = list(range(16))
+
+    def modality_config(self) -> dict[str, ModalityConfig]:
+        video_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.video_keys,
+        )
+
+        state_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.state_keys,
+        )
+
+        action_modality = ModalityConfig(
+            delta_indices=self.action_indices,
+            modality_keys=self.action_keys,
+        )
+
+        language_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.language_keys,
+        )
+
+        modality_configs = {
+            "video": video_modality,
+            "state": state_modality,
+            "action": action_modality,
+            "language": language_modality,
+        }
+
+        return modality_configs
+
+    def transform(self) -> ModalityTransform:
+        transforms = [
+            # video transforms
+            VideoToTensor(apply_to=self.video_keys),
+            VideoCrop(apply_to=self.video_keys, scale=0.95),
+            VideoResize(apply_to=self.video_keys, height=224, width=224, interpolation="linear"),
+            VideoColorJitter(
+                apply_to=self.video_keys,
+                brightness=0.3,
+                contrast=0.4,
+                saturation=0.5,
+                hue=0.08,
+            ),
+            VideoToNumpy(apply_to=self.video_keys),
+            # state transforms
+            StateActionToTensor(apply_to=self.state_keys),
+            StateActionTransform(
+                apply_to=self.state_keys,
+                normalization_modes={key: "min_max" for key in self.state_keys},
+            ),
+            # action transforms
+            StateActionToTensor(apply_to=self.action_keys),
+            StateActionTransform(
+                apply_to=self.action_keys,
+                normalization_modes={key: "min_max" for key in self.action_keys},
+            ),
+            # concat transforms
+            ConcatTransform(
+                video_concat_order=self.video_keys,
+                state_concat_order=self.state_keys,
+                action_concat_order=self.action_keys,
+            ),
+            # model-specific transform
+            GR00TTransform(
+                state_horizon=len(self.observation_indices),
+                action_horizon=len(self.action_indices),
+                max_state_dim=64,
+                max_action_dim=32,
+            ),
+        ]
+        return ComposedModalityTransform(transforms=transforms)
+
+
+###########################################################################################
+
+
 class Gr1FullUpperBodyDataConfig(BaseDataConfig):
     video_keys = ["video.front_view"]
     state_keys = [
@@ -982,4 +1080,5 @@ DATA_CONFIG_MAP = {
     "so100_wc": So100WCDataConfig(),
     "double_so100": DoubleSo100DataConfig(),
     "double_so100_wc": DoubleSo100WCDataConfig(),
+    "double_so100_w2c": DoubleSo100W2CDataConfig(),
 }
